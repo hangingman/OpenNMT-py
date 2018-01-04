655ec249e6663acd873c974dafe47a0f6c9afedc
Andre Martins <andre.t.martins@gmail.com>
2018-01-04 15:44:34 +0000

    Quick fix.

--- onmt/Models.py
+++ onmt/Models.py
@@ -469,7 +469,7 @@ class Decoder(nn.Module):
                 if self.exhaustion_loss:
                     attns["upper_bounds"] += [upper_bounds]
 	    if self.supervised_fertility:
-                if test: 
+                if not test:
                     attns["true_fertility_vals"] += [true_fertility_vals]
                 attns["predicted_fertility_vals"] += [predicted_fertility_vals]
             state = RNNDecoderState(hidden, output.unsqueeze(0),
