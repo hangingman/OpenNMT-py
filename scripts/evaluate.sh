69562510ddb9ac92f224aede12486df39cd74b85
Andre Martins <andre.t.martins@gmail.com>
2018-02-06 17:05:03 +0000

    Some cleaning,

--- scripts/evaluate.sh
+++ scripts/evaluate.sh
@@ -11,13 +11,14 @@ alpha=0 #0.2
 beta=0 #0.2
 #c_attn=0 #0.2
 #attn_transform=constrained_sparsemax
-#fertility_type=actual #guided
+fertility_type=predicted #actual #guided
 
 cd ..
 
 #python translate.py -model $model -src $source -output $target.pred -attn_transform constrained_softmax -guided_fertility $align -guided_fertility_source_file ${train_src} -beam_size 10 -alpha ${alpha} -beta ${beta} -replace_unk -verbose -gpu 3
 #python translate.py -model $model -src $source -output $target.pred -beam_size 10 -alpha ${alpha} -beta ${beta} -c_attn ${c_attn} -replace_unk -verbose -gpu 3
 python translate.py -model $model -src $source -output $target.pred -beam_size 10 -batch_size 1 -alpha ${alpha} -beta ${beta} -replace_unk -verbose -gpu 0
+#python translate.py -model $model -src $source -output $target.pred -beam_size 10 -batch_size 1 -alpha ${alpha} -beta ${beta} -fertility_type ${fertility_type} -replace_unk -verbose -gpu 0
 #python translate.py -model $model -src $source -output $target.pred -beam_size 10 -batch_size 1 -alpha ${alpha} -beta ${beta} -attn_transform ${attn_transform} -fertility_type ${fertility_type} -c_attn ${c_attn} -replace_unk -verbose -gpu 0
 sed -r 's/(@@ )|(@@ ?$)//g' $target.pred > $target.pred.merged
 perl multi-bleu.perl -lc $target < $target.pred.merged
