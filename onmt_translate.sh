1381610f0f63484ea4a24b0a8df3b301d633b064
PedroMLF <ferreira.ml.pedro@gmail.com>
2018-09-30 00:36:02 +0000

    Added the lazy translate scripts

--- onmt_translate.sh
+++ onmt_translate.sh
@@ -1,108 +1,81 @@
 # Generic things
-SOURCE=de
+SOURCE=es
 TARGET=en
 LANGPAIR=${SOURCE}-${TARGET}
 
-# Specific things to translation
-#MODEL_NAME=de-en-shr-base_acc_43.15_ppl_63.81_e9.pt
-#MODEL_NAME=/mnt/data/${LANGPAIR}-shr/base/de-en-md-shr_acc_79.67_ppl_2.58_e15.pt
-#MODEL_NAME=/mnt/data/${LANGPAIR}-shr/base/en-de-jrc-shr_acc_77.19_ppl_3.02_e15.pt
+# German to English in-domain models
+#MODEL_NAME=/mnt/data/${LANGPAIR}-md-shr/base/de-en-md-shr_acc_79.67_ppl_2.58_e15.pt
 #MODEL_NAME=/mnt/data/${LANGPAIR}-shr-big/base/de-en-shr-big_acc_55.25_ppl_12.96_e15.pt
-#MODEL_NAME=/mnt/data/${LANGPAIR}-md-shr-small-da-genvocab/5k/gen_indomain_5k_acc_47.22_ppl_36.32_e25.pt
-#MODEL_NAME=/mnt/data/${LANGPAIR}-md-shr-small-da-genvocab/10k/gen_indomain_10k_acc_48.82_ppl_32.45_e25.pt
-#MODEL_NAME=/mnt/data/${LANGPAIR}-md-shr-small-da-genvocab/15k/gen_indomain_15k_acc_49.84_ppl_29.81_e28.pt
-#MODEL_NAME=/mnt/data/${LANGPAIR}-md-shr-small-da-genvocab/20k/gen_indomain_20k_acc_50.55_ppl_27.94_e25.pt
-#MODEL_NAME=/mnt/data/${LANGPAIR}-md-shr-small-da-genvocab/30k/gen_indomain_30k_acc_51.45_ppl_25.25_e26.pt
-MODEL_NAME=/mnt/data/de-en-scnmt/csoftmax_predicted__0.8/preprocessed_csoftmax_predicted_cattn-0.8_acc_43.39_ppl_67.42_e10.pt
+
+# Spanish to English in-domain models
+MODEL_NAME=/mnt/data/${LANGPAIR}-md-shr/base/es-en-md-shr_acc_74.58_ppl_4.06_e14.pt
+#MODEL_NAME=/mnt/data/${LANGPAIR}-shr-big/base/es-en-shr-big_acc_68.32_ppl_4.82_e15.pt
 
 # Specify the source file
-#SRC_FILE=/mnt/data/${LANGPAIR}-md-shr-small-da-genvocab/test.bpe.sink.${SOURCE}
-SRC_FILE=/mnt/data/${LANGPAIR}-scnmt/test.bpe.sink.${SOURCE}
+SRC_FILE=/mnt/data/${LANGPAIR}-md-shr/test.bpe.${SOURCE}
 
 # Specify TP path
-TP_PATH=/mnt/translation_pieces/${LANGPAIR}-md-genvocab
+TP_PATH=/mnt/translation_pieces/${LANGPAIR}-md
 
-#for lambda1 in 3.5
-#do
-#    for extrald1 in 5.0
-#    do
- 
-#        if [ $(echo "${lambda1}==${lambda2}"|bc) -eq 1 ]
-#        then
-#            echo "Skipping ${lambda1} and ${lambda2}"
-#            continue
-#        fi
+# Hyperparameters
+lambda1=1.1
+lambda2=1.0
+extrald1=0.0
+extrald2=${extrald1}
+m=5
+th=0
 
-        lambda1=3.5
-        lambda2=4.0
-        extrald1=5.0
-        extrald2=5.0
-        m=20
-        th=3
-        suffix=1000000000000000000
-        fntmodel=30
+echo "Lambda1: ${lambda1}"
+echo "Lambda2: ${lambda2}"
+echo "Extrald1: ${extrald1}"
+echo "Extrald2: ${extrald2}"
+echo "M: ${m}" 
+echo "Threshold: 0.${th}"
 
-        echo "Lambda1: ${lambda1}"
-        echo "Lambda2: ${lambda2}"
-        echo "Extrald1: ${extrald1}"
-        echo "Extrald2: ${extrald2}"
-        echo "M: ${m}" 
-        echo "Threshold: 0.${th}"
-        echo "Finetuning: ${fntmodel}"
-        echo "Extra tps: ${suffix}"
- 
-        # Call the OpenNMT-py script
-        python3 translate.py \
-                -model ${MODEL_NAME} \
-                -src ${SRC_FILE} \
-                -output ${SRC_FILE}.pred \
-                -beam_size 5 \
-                -min_length 3 \
-                -fertility 2 \
-                -fertility_type "fixed" \
-                -replace_unk \
-                -gpu 0
-                #-use_guided \
-                #-tp_path ${TP_PATH}/test_translation_pieces_md_${m}-th0pt${th}_finetuning_${suffix}k.pickle \
-                #-guided_n_max 4 \
-                #-guided_1_weight ${lambda1} \
-                #-guided_n_weight ${lambda2} \
-                #-guided_correct_ngrams \
-                #-guided_correct_1grams \
-                #-extend_with_tp \
-                #-extend_1_weight ${extrald1} \
-                #-extend_n_weight ${extrald2} \
-                #-replace_unk
+# Call the OpenNMT-py script
+python3 translate.py \
+        -model ${MODEL_NAME} \
+        -src ${SRC_FILE} \
+        -output ${SRC_FILE}.pred \
+        -beam_size 5 \
+        -min_length 3 \
+        -use_guided \
+        -tp_path ${TP_PATH}/test_translation_pieces_md_${m}-th0pt${th}.pickle-high \
+        -guided_n_max 4 \
+        -guided_1_weight ${lambda1} \
+        -guided_n_weight ${lambda2} \
+        -guided_correct_ngrams \
+        -guided_correct_1grams \
+        -replace_unk
+        #-extend_with_tp \
+        #-extend_1_weight ${extrald1} \
+        #-extend_n_weight ${extrald2} \
+        #-replace_unk
+# CHANGE THE NAME OF THE FILE
 
-        # CHANGE THE NAME OF THE FILE
-    
-        # Copy the predictions to the right folders
-        HOME_PATH=/home/ubuntu/NMT-Code/attention_comparison/thesis/guided_nmt
-        PRED_PATH=${HOME_PATH}/generate_results_de_en_da/preds
-        MT_PATH=${HOME_PATH}/generate_results_de_en_da/mt_predictions
+# Copy the predictions to the right folders
+HOME_PATH=/home/ubuntu/NMT-Code/attention_comparison/thesis/guided_nmt
+PRED_PATH=${HOME_PATH}/generate_results_es_en_md/preds
+MT_PATH=${HOME_PATH}/generate_results_es_en_md/mt_predictions
 
-        v11=$(echo ${lambda1} | cut -f1 -d.)
-        v12=$(echo ${lambda1} | cut -f2 -d.)
-        v21=$(echo ${lambda2} | cut -f1 -d.)
-        v22=$(echo ${lambda2} | cut -f2 -d.)
-        
-        v31=$(echo ${extrald1} | cut -f1 -d.)
-        v32=$(echo ${extrald1} | cut -f2 -d.)
-        v41=$(echo ${extrald2} | cut -f1 -d.)
-        v42=$(echo ${extrald2} | cut -f2 -d.)
-        
-        #POS=guided-${m}-0pt${th}-lambda-${v11}pt${v12}-${v21}pt${v22}-extrald-${v31}pt${v32}-${v41}pt${v42}-dynamic_unk-mod45-finetuned_${fntmodel}k-extra_${suffix}k
-        #POS=guided-${m}-0pt${th}-lambda-${v11}pt${v12}-${v21}pt${v22}-extrald-${v31}pt${v32}-${v41}pt${v42}-dynamic_unk-mod45-finetuned_${fntmodel}k
-        #POS=guided-${m}-0pt${th}-lambda-${v11}pt${v12}-${v21}pt${v22}-extrald-${v31}pt${v32}-${v41}pt${v42}-dynamic_unk-mod45-extra_${suffix}k
-        #POS=guided-10-0pt0-lambda-${v11}pt${v12}-${v21}pt${v22}
-        #POS=teste
-        
-        #FN=$(echo ${SRC_FILE} | cut -d'/' -f5)
-        #cp ${SRC_FILE}.pred ${PRED_PATH}/${FN}.pred.${POS}
-        #sed -r 's/(@@ )|(@@ ?$)//g' ${PRED_PATH}/${FN}.pred.${POS} > \
-        #                            ${MT_PATH}/${FN}.pred.${POS}.merged
+v11=$(echo ${lambda1} | cut -f1 -d.)
+v12=$(echo ${lambda1} | cut -f2 -d.)
+v21=$(echo ${lambda2} | cut -f1 -d.)
+v22=$(echo ${lambda2} | cut -f2 -d.)
 
-#    done
-#done
+v31=$(echo ${extrald1} | cut -f1 -d.)
+v32=$(echo ${extrald1} | cut -f2 -d.)
+v41=$(echo ${extrald2} | cut -f1 -d.)
+v42=$(echo ${extrald2} | cut -f2 -d.)
+
+POS=guided-${m}-0pt${th}-lambda-${v11}pt${v12}-${v21}pt${v22}-extrald-${v31}pt${v32}-${v41}pt${v42}-dynamic_unk-high
+#POS=guided-${m}-0pt${th}-lambda-${v11}pt${v12}-${v21}pt${v22}
+#POS=base
+
+FN=$(echo ${SRC_FILE} | cut -d'/' -f5)
+cp ${SRC_FILE}.pred ${PRED_PATH}/${FN}.pred.${POS}
+sed -r 's/(@@ )|(@@ ?$)//g' ${PRED_PATH}/${FN}.pred.${POS} > \
+                            ${MT_PATH}/${FN}.pred.${POS}.merged
+
 
 date +"%F %T,%3N"
--- onmt_translate_scnmt.sh
+++ onmt_translate_scnmt.sh
@@ -1,14 +1,25 @@
+echo "Initial Time:"
+date +"%F %T,%3N"
+
 # Generic things
 SOURCE=de
 TARGET=en
 LANGPAIR=${SOURCE}-${TARGET}
 
 # German to English in-domain models
-#MODEL_NAME=/mnt/data/${LANGPAIR}-scnmt/csoftmax_predicted__0.8/preprocessed_csoftmax_predicted_cattn-0.8_acc_43.81_ppl_68.65_e13.pt
-MODEL_NAME=/mnt/data/${LANGPAIR}-scnmt/csparsemax_predicted__0.8/preprocessed_csparsemax_predicted_cattn-0.8_acc_28.38_ppl_221.98_e1.pt
+MODEL_PREFIX=/mnt/data/${LANGPAIR}-scnmt
+#MODEL_NAME=${MODEL_PREFIX}/csoftmax/used/preprocessed_csoftmax_fixed-2_cattn-0.0_acc_42.87_ppl_67.23_e9.pt
+MODEL_NAME=${MODEL_PREFIX}/softmax/preprocessed_softmax_cattn-0_acc_42.90_ppl_68.61_e9.pt
+#MODEL_NAME=${MODEL_PREFIX}/sparsemax/preprocessed_sparsemax_cattn-0_acc_42.98_ppl_66.92_e9.pt
+#MODEL_NAME=${MODEL_PREFIX}/csparsemax/used/preprocessed_csparsemax_actual_cattn-0.0_acc_43.18_ppl_71.96_e9.pt
+#MODEL_NAME=${MODEL_PREFIX}/csparsemax/used/preprocessed_csparsemax_fixed-2_cattn-0.0_acc_43.64_ppl_68.81_e10.pt
+#MODEL_NAME=${MODEL_PREFIX}/csparsemax/used/preprocessed_csparsemax_fixed-3_cattn-0.0_acc_43.70_ppl_69.74_e10.pt
+#MODEL_NAME=${MODEL_PREFIX}/csparsemax/used/preprocessed_csparsemax_guided_cattn-0.0_acc_43.88_ppl_68.62_e11.pt
+
 
 # Specify the source file
-SRC_FILE=/mnt/data/${LANGPAIR}-scnmt/test.bpe.sink.${SOURCE}
+#SRC_FILE=/mnt/data/${LANGPAIR}-scnmt/test.bpe.sink.${SOURCE}
+SRC_FILE=/mnt/data/${LANGPAIR}-scnmt/dev.bpe.sink.${SOURCE}
 
 # Call the OpenNMT-py script
 python translate.py \
@@ -17,22 +28,23 @@ python translate.py \
         -output ${SRC_FILE}.pred \
         -beam_size 5 \
         -batch_size 1 \
+        -min_length 3 \
         -min_attention 0.1 \
         -replace_unk \
         -gpu 0
 # CHANGE THE NAME OF THE FILE
 
 # Copy the predictions to the right folders
-HOME_PATH=/home/ubuntu/NMT-Code/attention_comparison/thesis/guided_nmt
-PRED_PATH=${HOME_PATH}/generate_results_de_enX/preds
-MT_PATH=${HOME_PATH}/generate_results_de_enX/mt_predictions
+HOME_PATH=/home/ubuntu/NMT-Code/attention_comparison/thesis/constrained_sparse_experiments
+PRED_PATH=${HOME_PATH}/generate_results_de_en/preds
+MT_PATH=${HOME_PATH}/generate_results_de_en/mt_predictions
 
-#POS=base
+POS=softmax
 
-#FN=$(echo ${SRC_FILE} | cut -d'/' -f5)
-#cp ${SRC_FILE}.pred ${PRED_PATH}/${FN}.pred.${POS}
-#sed -r 's/(@@ )|(@@ ?$)//g' ${PRED_PATH}/${FN}.pred.${POS} > \
-#                            ${MT_PATH}/${FN}.pred.${POS}.merged
+FN=$(echo ${SRC_FILE} | cut -d'/' -f5)
+cp ${SRC_FILE}.pred ${PRED_PATH}/${FN}.pred.${POS}
+sed -r 's/(@@ )|(@@ ?$)//g' ${PRED_PATH}/${FN}.pred.${POS} > \
+                            ${MT_PATH}/${FN}.pred.${POS}.merged
 
-
+echo "Finishing Time:"
 date +"%F %T,%3N"
